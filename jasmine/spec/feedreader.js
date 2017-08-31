6a98bd522905105f0e23d5329512d68085b8c59c
Brandon Smith <Brandon@reddirtwebdesign.com>
2017-08-30 17:24:17 -0700

    Added two comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -121,6 +121,8 @@ $(function () {
         var oldFeed;
         var newFeed;
         beforeEach(function (done) {
+            
+            //Run the load feed function and check what links are currently present
             loadFeed(0, function () {
                 oldFeed = $('.entry-link').text();
                 return oldFeed
@@ -129,6 +131,7 @@ $(function () {
         })
 
         it('Load new content when the content is changed', function (done) {
+            //Run the load feed function again and check which links are now present
             loadFeed(1, function () {
                 newFeed = $('.entry-link').text();
                 return newFeed
